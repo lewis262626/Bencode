112142047d3a3c144184c32273569937d8e29520
Lewis Robbins <lewis.robbins2@gmail.com>
2020-01-08 18:18:12 +0000

    done byte string and int decoding

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 with open('ubuntu-19.10-desktop-amd64.iso.torrent', 'rb') as fp:
     read_data = fp.read()
 
-print(read_data)
+def decode_int(data):
+    if data[0].to_bytes(1, byteorder='big') == b'i':
+        num = data[1:len(data)-1]
+        return num.decode('ascii')
+    else:
+        raise TypeError('not valid bencode')
+
+def decode_str(data):
+    for i, byte in enumerate(data):
+        if byte.to_bytes(1, byteorder='big') == b':':
+            return data[i+1:len(data)]
+
+
+print(decode_int(b'i42e'))
+print(decode_str(b'4:spam'))
